acc976ab1b08af7eb5fccff87f678d40760bfabd
Cosme Sousa <36869385+Cosmess@users.noreply.github.com>
2025-04-06 00:32:27 -0300

    teste

--- src/infrastructure/firebase/firebase.module.ts
+++ src/infrastructure/firebase/firebase.module.ts
@@ -12,7 +12,7 @@ process.env.GRPC_SSL_CIPHER_SUITES = 'HIGH+ECDSA';
         return admin.initializeApp({
           credential: admin.credential.cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, '\n'),
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
           }),
           databaseURL: process.env.FIREBASE_DATABASE_URL,
