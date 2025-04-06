0cdef7ec5298817718ef939be13b01ad7f7366fa
Cosme Sousa <36869385+Cosmess@users.noreply.github.com>
2025-04-06 00:38:22 -0300

    ajuste

--- src/infrastructure/firebase/firebase.module.ts
+++ src/infrastructure/firebase/firebase.module.ts
@@ -1,26 +1,8 @@
 import { Module } from '@nestjs/common';
-import * as admin from 'firebase-admin';
 import { FirebaseService } from './firebase.service';
-process.env.FIRESTORE_USE_REST = 'true';
-process.env.GRPC_SSL_CIPHER_SUITES = 'HIGH+ECDSA';
-console.log('FIREBASE_PRIVATE_KEY', process.env.FIREBASE_PRIVATE_KEY);
+
 @Module({
-  providers: [
-    {
-      provide: 'FIREBASE_ADMIN',
-      useFactory: () => {
-        return admin.initializeApp({
-          credential: admin.credential.cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, '\n'),
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          }),
-          databaseURL: process.env.FIREBASE_DATABASE_URL,
-        });
-      },
-    },
-    FirebaseService,
-  ],
-  exports: ['FIREBASE_ADMIN', FirebaseService], // ✅ Certifique-se de que o FirebaseService está exportado
+  providers: [FirebaseService],
+  exports: [FirebaseService],
 })
 export class FirebaseModule {}
--- src/infrastructure/firebase/firebase.service.ts
+++ src/infrastructure/firebase/firebase.service.ts
@@ -1,11 +1,22 @@
-import { Injectable, Inject } from '@nestjs/common';
-import * as admin from 'firebase-admin';
+import { Injectable } from '@nestjs/common';
+import { Firestore } from '@google-cloud/firestore';
 
 @Injectable()
 export class FirebaseService {
-  constructor(@Inject('FIREBASE_ADMIN') private readonly firebaseApp: admin.app.App) {}
+  private firestore: Firestore;
+
+  constructor() {
+    this.firestore = new Firestore({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      credentials: {
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      },
+      preferRest: true, // 👈 ESSENCIAL no Heroku
+    });
+  }
 
   getFirestore() {
-    return this.firebaseApp.firestore();
+    return this.firestore;
   }
 }
